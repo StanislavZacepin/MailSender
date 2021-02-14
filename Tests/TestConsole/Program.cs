e8e94228d2dcd0777a7ca8f935102ff7f4c00ab0
TouchPad <olsher258@mail.ru>
2021-02-14 13:02:19 +0300

    На консоле сделалано первая часть

--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Net;
+using System.Net.Mail;
+using TestConsole.model;
 
 namespace TestConsole
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var client = new SmtpClient(WpfTestMailSender.AdresServer, WpfTestMailSender.portServer);
+            client.EnableSsl = true;
+            client.Credentials = new NetworkCredential
+            {
+                UserName = WpfTestMailSender.UserName,
+                Password = WpfTestMailSender.Pasword
+            };
+            var message = new MailMessage(WpfTestMailSender.From, WpfTestMailSender.to);
+            message.Subject = WpfTestMailSender.Subject;
+            message.Body = WpfTestMailSender.Body;
+            client.Send(message);
         }
     }
 }
---
+++ Tests/TestConsole/model/WpfTestMailSender.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Mail;
+
+namespace TestConsole.model
+{
+    public static class WpfTestMailSender
+    {
+        public static string AdresServer = "smtp.yandex.ru";
+        public  static int portServer = 465;
+        public static string UserName = "sadasd";
+        public static string Pasword = "sfdsf";
+        #region MailAdress
+        public static MailAddress From = new MailAddress("olsher92@mail.ru", "Стас");
+        public static MailAddress to = new MailAddress("olsher258@mail.ru", "Стас");
+        #endregion
+        
+        public static string Subject = "Заголовок";
+        public static string Body = "Текст писма";
+
+    }
+}
