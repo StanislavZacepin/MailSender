859caa16f760efdbfef0edb2a1783751f258bb8f
TouchPad <olsher258@mail.ru>
2021-02-22 15:45:05 +0300

    Модэли данных

---
+++ Tests/TestWPF/Models/Recipent.cs
@@ -0,0 +1,8 @@
+﻿namespace WpfMailSender.Models
+{
+    public class Recipent
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ Tests/TestWPF/Models/Server.cs
@@ -0,0 +1,12 @@
+﻿namespace WpfMailSender.Models
+{
+    public class Server
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Port { get; set; }
+        public bool UseSSL { get; set; }
+        public bool Login { get; set; }
+        public bool Password { get; set; }
+    }
+}
