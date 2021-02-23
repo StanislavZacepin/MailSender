baa3493788a86016ee40849574fc2ce249a3620c
TouchPad <olsher258@mail.ru>
2021-02-23 17:01:05 +0300

    Модель-Представления главного окна основного проекта

---
+++ Tests/TestWPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MailSender.lib.ViewModels.Base;
+
+namespace WpfMailSender.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        private string _Title = "Рассыльщик";
+
+        public string Title { get => _Title; set => Set(ref _Title, value);}
+
+        private string _Status = "Готов!";
+
+        public string Status { get => _Status; set => Set(ref _Status, value); }
+    }
+}
