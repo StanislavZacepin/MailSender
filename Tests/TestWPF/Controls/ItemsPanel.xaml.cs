1e65954b6c17f33b030cacfe1c88930d0466ad1b
TouchPad <olsher258@mail.ru>
2021-02-23 12:35:59 +0300

    Свойство-зависимости и привязка данных

---
+++ Tests/TestWPF/Controls/ItemsPanel.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfMailSender.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для ItemsPanel.xaml
+    /// </summary>
+    public partial class ItemsPanel : UserControl
+    {
+        public static readonly DependencyProperty TitleProperty = 
+            DependencyProperty.Register(
+                nameof(Title),
+                typeof(string),
+                typeof(ItemsPanel),
+                new PropertyMetadata(default(string),OnTitleChanged));
+
+        private static void OnTitleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+           
+        }
+
+        public string Title 
+        { 
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty,value);
+        }
+        public ItemsPanel() => InitializeComponent();
+    }
+}
