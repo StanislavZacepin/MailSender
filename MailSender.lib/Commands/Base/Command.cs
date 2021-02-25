00085dec60f7033fcbd3dcd46ef8c82797f8cab5
TouchPad <olsher258@mail.ru>
2021-02-25 21:06:55 +0300

    Проблема решилась. надо было собрать проект . и сделал команду закрытия

--- MailSender.lib/Commands/Base/Command.cs
+++ MailSender.lib/Commands/Base/Command.cs
@@ -16,11 +16,9 @@ namespace MailSender.lib.Commands.Base
         }
 
         public virtual bool CanExecute(object parameter) => true;
-       
 
-        public void Execute(object parameter)
-        {
-            throw new NotImplementedException();
-        }
+
+        public abstract void Execute(object parameter);
+       
     }
 }
